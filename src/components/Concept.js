9a394586226325de28f6aef05dbc1e737a612d47
Celal Karakoc <c.karakoc@live.nl>
2023-07-13 20:18:10 +0200

    :tada: Initial commit!

---
+++ src/components/Concept.js
@@ -0,0 +1,9 @@
+import './Concept.css'
+
+export default function Concept(props) {
+    return <li className="concept">
+            <img src={props.concept.image} alt={props.concept.title} />
+            <h2>{props.concept.title}</h2>
+            <p>{props.concept.description}</p>
+        </li>;
+}
